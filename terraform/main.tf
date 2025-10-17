daa3566c5e0ac6eedcb11c32254635ad59280d91
Joe Strickland <joseph.a.strickland@gmail.com>
2025-10-17 10:31:14 -0400

    Added more resources and updated variables.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,9 @@
-data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {
+}
 
 module "vpc" {
   source              = "./modules/infra/network/vpc"
-  region              = var.region
+  region              = var.aws_region
   environment         = var.environment
   asset_owner_name    = var.asset_owner_name
   team_name           = var.team_name
@@ -17,10 +18,12 @@ module "vpc" {
 
 module "s3_bucket" {
   source             = "./modules/infra/storage/s3"
-  region             = var.region
+  aws_region         = var.aws_region
+  aws_profile        = var.aws_profile
   asset_owner_name   = var.asset_owner_name
-  bucket_name        = var.team_name
+  bucket_name        = "${var.team_name}-${var.environment}-bucket"
   s3_vpc_endpoint_id = module.vpc.s3_vpc_endpoint_id
+  allowed_ips        = var.allowed_ips
 }
 
 module "key_pair" {
@@ -30,8 +33,15 @@ module "key_pair" {
   asset_owner_name = var.asset_owner_name
 }
 
+module "s3_ro_role" {
+  source            = "./modules/security/roles/ec2/s3_ro_role"
+  ec2_aws_role_name = "${var.team_name}-ec2-s3-ro-role"
+  s3_arn            = [module.s3_bucket.bucket_arn, "${module.s3_bucket.bucket_arn}/*"]
+  team_name         = var.team_name
+}
+
 module "security_groups" {
-  source              = "./modules/networking/security_groups"
+  source              = "./modules/infra/network/security_groups"
   asset_owner_name    = var.asset_owner_name
   vpc_id              = module.vpc.vpc_id
   trusted_ips         = var.trusted_ips
@@ -75,20 +85,33 @@ module "linux_conector" {
 }
 */
 module "linux_target" {
-  source                        = "./modules/infra/compute/ec2/targets/linux_target"
-  iCreateor_CreatorBy           = var.iCreateor_CreatorBy
-  environment                   = var.environment
-  team_name                     = var.team_name
-  name                          = "${var.team_name}-linux-target"
-  asset_owner_name              = var.asset_owner_name
-  key_name                      = module.key_pair.key_name
-  iScheduler                    = var.iScheduler
-  linux_security_group_ids      = module.security_groups.ssh_internal_flat_sg_id
-  private_subnet_id             = module.vpc.private_subnet_id
-  linux_hostname                = var.linux_hostname
-  linux_instance_type           = var.linux_instance_type
+  source                          = "./modules/infra/compute/ec2/targets/linux_target"
+  iCreator_CreatorBy              = var.iCreator_CreatorBy
+  environment                     = var.environment
+  team_name                       = var.team_name
+  name                            = "${var.team_name}-linux-target"
+  asset_owner_name                = var.asset_owner_name
+  key_name                        = module.key_pair.key_name
+  iScheduler                      = var.iScheduler
+  linux_security_group_ids        = [module.security_groups.ssh_internal_flat_sg_id]
+  private_subnet_id               = module.vpc.private_subnet_id
+  linux_hostname                  = var.linux_hostname
+  linux_instance_type             = var.linux_instance_type
+  s3_bucket_name                  = module.s3_bucket.bucket_name
+  s3_bucket_path                  = var.ubuntu_scripts_s3_bucket_path
+  ec2_s3_ro_instance_profile_name = module.s3_ro_role.profile_name
+  aws_role_name                   = "${var.team_name}-ec2-s3-ro-role"
+  username_variable               = var.username_variable
+  password_variable               = var.password_variable
+  service_id                      = var.service_id
+  host_id                         = var.host_id
+  platform_tenant_name            = var.platform_tenant_name
+  identity_tenant_id              = var.identity_tenant_id
+  workspace_id                    = var.workspace_id
+  workspace_type                  = var.workspace_type
 }
 
+/*
 module "mysql_subnet_group" {
   source             = "./modules/infra/network/subnets/database/rds/mysql_subnet_group"
   team_name          = var.team_name
@@ -105,4 +128,4 @@ module "mysql" {
   db_subnet_group_name   = module.mysql_subnet_group.mysql_subnet_group_name
   asset_owner_name       = var.asset_owner_name
   vpc_security_group_ids = [module.security_groups.mysql_target_sg_id]
-}
+}*/
